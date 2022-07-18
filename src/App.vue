12fe29651da2da54d436efefeabbf6e891822598
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-07-18 16:35:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  
+    <LandingView />
+  
+    <div class="container min-vh-100">
+      <H1>My skills</H1>
+    </div>
+
+    <div class="container min-vh-100" >
+      <H1>My projects</H1>
+    </div>
+
+    <div class="container min-vh-100">
+      <H1>My footer</H1>
+    </div>
+
+  <div>
+    <FooterView />
+  </div>
+    
+
+  <router-view/>
+
+</template>
+
+<script>
+import LandingView from './views/LandingView.vue'
+import FooterView from './views/FooterView.vue'
+
+export default {
+  components: { LandingView, FooterView },
+
+  
+}
+</script>
+
+<!-- styles -->
+<style>
+
+</style>
---
+++ src/views/FooterView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="footer">
+        <div class="row">
+            <div class="col-4">rtyu</div>
+            <div class="col-4">wertyu</div>
+            <div class="col-4">wertyu</div>
+        </div>
+    </div>
+</template>
+
+<style>
+#footer{
+    background-color: #ffc2cc;
+    height: 20vh;
+}
+</style>
