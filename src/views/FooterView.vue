b914a7939edbcbcc6aaccf3430e244dce2d16605
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-09-19 13:43:00 +0200

    test

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -2,19 +2,33 @@
   <div id="footer" style="padding-top: 10rem;">
     <div id="footerInfo">
     <div class="container">
-      <div class="d-flex justify-content-evenly" style="padding-top: 3rem">
+      <div class="d-flex justify-content-center" style="padding-top: 3rem">
 
-      <a href="https://github.com/bulelaniskhikhi" target="_blank">
+        <div class="row" style="width: 100%; ">
+          <div class="col-6" style="display: flex; justify-content: center;">
+<a href="https://github.com/bulelaniskhikhi" target="_blank">
         <i class="fa-brands fa-github"></i>
       </a>
-        
-        <a href="https://www.linkedin.com/in/zuiel-undefined-59040a246/" target="_blank">
+          </div>
+
+          <div class="col-6" style="display: flex; justify-content: center;">
+            <a href="https://www.linkedin.com/in/zuiel-undefined-59040a246/" target="_blank">
           <i class="fa-brands fa-linkedin"></i>
         </a>
-        
-        <p style="color: white; font-size: 30px"><i class="fa-solid fa-phone"></i> +27 84 820 5707</p>
-      </div>
+          </div>
 
+        </div>
+        
+      </div>
+      <div class="row" >
+        
+          <div class="col-12">
+            <p style="color: white; font-size: 30px; display: flex; justify-content: center;"><i class="fa-solid fa-phone"></i> +27 84 820 5707</p>
+          </div>
+        
+     
+    
+  </div>
       <div style="width: 100%; text-align: center">
         <p class="footertext fw-light" style="font-size: 2rem">
           Xena Skhikhi copyright© 2022
